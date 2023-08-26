773e91546f37ccd34379006c1e09605d9379b2bc
Max Williams <max@welcometomonday.com>
2023-08-25 22:06:35 -0700

    fleshed out weapon sheet and card

--- src/module/partial/weapon-card/weaponCard.ts
+++ src/module/partial/weapon-card/weaponCard.ts
@@ -7,15 +7,20 @@ export default
 		<div class="title-row">
 			{{name}}
 			<div class="header-buttons">
+				<i class="fa-solid fa-dice-d20 weapon-roll" title="Roll Weapon" data-item-id="{{id}}"></i>
 				<i class="fa-solid fa-pen-to-square item-edit" title="Edit Weapon" data-item-id="{{id}}"></i>
 			</div>
 		</div>
-		{{#if tags}}
+		<div class="type">
+			{{system.size}} {{system.type}}
+		</div>
+		{{#if system.tags}}
 		<div class="tags">
+			{{system.tags}}
 		</div>
 		{{/if}}
 		<div class="traits">
-			{{#if system.range}}[Range: {{system.range}}]{{/if}} {{#if system.damage.value}}[Damage: {{system.damage.value}} ({{system.damage.type}})]{{/if}}
+			{{#if system.range}}[Range: {{system.range}}]{{/if}} {{#if system.threat}}[Threat: {{system.threat}}]{{/if}} {{#if system.damage}}[Damage: {{system.damage}}]{{/if}}
 		</div>
 	</div>
 	<div class="body">
